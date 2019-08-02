38ce189c036433ab3f751bb6b9761b716ea574a4
ccc <522978139@qq.com>
2019-08-02 08:55:22 +0800

    demo3-1 输出管理

--- demo3-1/webpack.config.js
+++ demo3-1/webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+/*
+ * clean-webpack-plugin 的用法变了，和webpack官方demo已经不一致了
+ * https://github.com/johnagan/clean-webpack-plugin#options-and-defaults-optional
+ */
 
 module.exports = {
     entry: {
@@ -7,6 +14,7 @@ module.exports = {
         print: './src/print.js'
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Output Management 管理输出'
         })
